d44cedc6153acd6bb4313251d79fea5d43dc2852
Darren Liau <liaukaize@gmail.com>
2022-08-31 13:50:42 +0800

    small update
    changed color scheme
    added relevant tags
    changed font

--- mainpage.php
+++ mainpage.php
@@ -1,13 +1,23 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <style>
-/* Content Main */
-#main{
+
+.main{
 	text-align: center;
-	background-color: white;
-	padding: 50px 0px 10px 0px;
-	font-weight: bold;
-	font-size: 22px;
+	background-color: rgb(0, 72, 106);
+	padding: 20px 10px;
+	font-size: 170%;
+	color: rgb(255, 255, 255);
+	/* font-family: Arial Narrow, Arial, sans-serif; */
+	font-family: Candara, Calibri, Segoe, Segoe UI, Optima, Arial, sans-serif;
+	/* font-family: Geneva, Tahoma, Verdana, sans-serif; */
+	border-radius: 8px;
 }
 
 </style>
@@ -15,16 +25,15 @@
 <body>
 
 
-<div id="main">
+<div class="main">
 
 	<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet recusandae nostrum ab facere vitae molestias ullam asperiores iste, magnam laboriosam excepturi aliquid eaque earum aliquam quidem illum? Magni, eaque est.
 	</div> <br>
 
 	<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsam harum doloribus illo numquam provident accusantium cumque quibusdam. Facilis deserunt dignissimos ipsam necessitatibus nihil repudiandae minus blanditiis reiciendis animi architecto!</div> <br>
 
-	<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsam harum doloribus illo numquam provident accusantium cumque quibusdam. Facilis deserunt dignissimos ipsam necessitatibus nihil repudiandae minus blanditiis reiciendis animi architecto!</div> <br>
+	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, ipsum quo? Tempora provident iusto nisi, pariatur officiis fuga recusandae asperiores reprehenderit, repellat architecto, repellendus eos error consequuntur fugiat. A, quia!</div>
 </div>
 
 </body>
 </html>
-	
