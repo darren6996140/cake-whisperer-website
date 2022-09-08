c9c1fbd1f470668a131f58c45f5fd891c651f461
Darren Liau <liaukaize@gmail.com>
2022-09-08 14:31:57 +0800

    lel lel lel

---
+++ privacy.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<style>
+
+        .privacy{
+            padding: 2%;
+            font-size: 250%;
+            font-weight: bold;
+        }
+
+        @media screen and (max-width: 850px) {
+			.privacy{
+				font-size: 100%;
+			}
+		}
+
+	</style>
+	
+</head>
+
+<body>
+    <?php
+    include("header.php");
+    ?>
+
+    <div class="privacy">
+        <p>
+            The Cake Whisperer ("The Company") will absolutely collect and sell all your data for optimal monetary benefits. Thank you and have fun learning!
+        </p>
+    </div>
+
+    <?php
+    include("footer.php")
+    ?>
+</body>
+
+</html>
---
+++ workworkwork.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<style>
+
+        .work{
+            padding: 2%;
+            font-size: 350%;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        @media screen and (max-width: 850px) {
+			.work{
+				font-size: 100%;
+			}
+		}
+
+	</style>
+	
+</head>
+
+<body>
+    <?php
+    include("header.php");
+    ?>
+
+    <div class="work">
+        <p>
+            We are currently not accepting any work offers.
+        </p>
+    </div>
+
+    <?php
+    include("footer.php")
+    ?>
+</body>
+
+</html>
