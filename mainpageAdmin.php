79bffecab75c5acf07dbca31cb75998d2faa78ee
Darren Liau <liaukaize@gmail.com>
2022-09-17 18:54:47 +0800

    added admin page

---
+++ mainpageAdmin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("session.php");
+include("header.php");
+?>
+<head>
+
+	<style>
+
+	</style>
+	
+</head>
+
+<body>
+
+	<center><h1>Salam Admin</h1></center>
+
+</body>
+<?php
+include("footer.php");
+?>
+</html>
